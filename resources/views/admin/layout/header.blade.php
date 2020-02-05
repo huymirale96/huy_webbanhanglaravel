971ebb4bfec40a231cd046efd1d3da4a470d11b0
SaoBV <buivansaobg@gmail.com>
2020-02-05 20:34:10 +0700

    config href

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <base href="{{asset('public/asset/admin')}}/">
+    <base href="/public/asset/admin/">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"/>
     <link rel="stylesheet"
