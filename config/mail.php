3f1ab1942e0697b43438e3ea0585c5d2ab1130b1
SaoBV <buivansaobg@gmail.com>
2020-02-12 21:21:40 +0700

    clear env

--- config/mail.php
+++ config/mail.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    "driver" => "smtp",
-    "host" => "smtp.mailtrap.io",
+    "driver" => env("MAIL_DRIVER", ""),
+    "host" => env("MAIL_HOST", ""),
     "port" => 2525,
     "from" => array(
-        "address" => "buivansao@tech.com",
-        "name" => "BUIVANSAO"
+        "address" => env("MAIL_FROM_ADDRESS", 'laravel'),
+        "name" => env("MAIL_FROM_NAME", 'laravel'),
     ),
-    "username" => "478f0b0328fca2",
-    "password" => "40fe688a4d2d81",
+    "username" => env("MAIL_USERNAME", ''),
+    "password" => env("MAIL_PASSWORD", ''),
     "sendmail" => "/usr/sbin/sendmail -bs"
 ];
